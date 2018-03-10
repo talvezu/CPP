4c883525e9ae4d7d9ce7978b6869bfa51e6cbbc3
nir <talvezu@walla.com>
2018-03-10 15:20:05 +0200

    Finner handling, with better handling and condition_variable

--- rt_queue_with_special_burst_handling/running_rt_queue.cpp
+++ rt_queue_with_special_burst_handling/running_rt_queue.cpp
@@ -5,8 +5,24 @@
 #include <iostream>
 #include <string>
 #include <mutex>
-
-
+#include <condition_variable>
+// ===============================
+// AUTHOR      : Nir Levy
+// CREATE DATE : 10/03/2018
+// PURPOSE     : supplying realtime cyclic buffer (mutex free) 
+//                  once buffer is full due to burstiness, close to RT handling (mutex + condition_variable) in stl::queue.
+//               objects - rt_queue_data (can be modified)
+//               RT_QUEUE_SIZE is the queue data
+//               
+//               Burstiness handling is only when cyclic buffer is full and push is required.
+//               once reader try_pop it checks only for extra_buffer_in_use flag.
+//               in case of extra_buffer_in_use equal false it do it's cyclic routine.
+//               when 
+// SPECIAL NOTES:
+// ===============================
+// Change History:
+//
+//==================================
 struct rt_queue_data {
 	char data[40] = { 0 };
 	struct rt_queue_data(std::string const& message) {
@@ -21,6 +37,7 @@ class rt_queue
 {
 	unsigned int reader_pos;
 	unsigned int writer_pos;
+	bool extra_buffer_in_use;
 	unsigned int max_rt_elements;
 	
 	/*REAL-TIME buffer*/
@@ -28,79 +45,75 @@ class rt_queue
 
 	/*Burst handling buffer*/
 	std::queue<T> extra_data;
-	std::mutex extra_data_mutex;
+	mutable std::mutex extra_data_mutex;
+	std::condition_variable extra_data_cond;
 public:
 	rt_queue() {}
 	~rt_queue() {}
+
 	void init(int max_rt_elements) {
 		reader_pos = 0;
 		writer_pos = 0;
+		extra_buffer_in_use = false;
 		cyclic_buffer = new T[max_rt_elements];
 		this->max_rt_elements = max_rt_elements;
 	}
 
-	void push(T&& new_element) {
-		unsigned int next_writer_pos = writer_pos + 1;
-		if (next_writer_pos > max_rt_elements) {
-			next_writer_pos = 0;
-		}
-		if (next_writer_pos == reader_pos )
-		{
-			printf("extra_data! pushing new element rhs %s\n", new_element.data);
-			std::lock_guard<std::mutex> lk(extra_data_mutex);
-			extra_data.push(new_element);
-		}
-		else
-		{
-			printf("push new element rhs %s\n", new_element.data);
-			cyclic_buffer[writer_pos] = new_element;
-			writer_pos = next_writer_pos;
-		}
-	}
 
 	void push(T& new_element) {
 		unsigned int next_writer_pos = writer_pos + 1;
-		if (next_writer_pos > max_rt_elements) {
+		if (next_writer_pos >= max_rt_elements) {
 			next_writer_pos = 0;
 		}
 		if (next_writer_pos == reader_pos)
-		{
-			printf("extra_data! pushing new element rhs %s\n", new_element.data);
+		{			
 			std::lock_guard<std::mutex> lk(extra_data_mutex);
+			//printf("extra_data! pushing new element rhs %s\n", new_element.data);
 			extra_data.push(new_element);
+			extra_buffer_in_use = true;
+			extra_data_cond.notify_all();			
 		}
 		else
 		{
-			printf("push new element rhs %s\n", new_element.data);
+			//printf("push new element rhs %s\n", new_element.data);
 			cyclic_buffer[writer_pos] = new_element;
 			writer_pos = next_writer_pos;
 		}
 	}
 
 
-	std::shared_ptr<T> pop() {
+	bool try_pop(T& value){
 		printf("pop element\n");
 
-		if (reader_pos == writer_pos)
-			return NULL;
+		if ((reader_pos == writer_pos) && !extra_buffer_in_use)
+			return false;
 
 		unsigned int next_reader_pos = reader_pos + 1;
 		if (next_reader_pos >= max_rt_elements) {
 			next_reader_pos = 0;
 		}
-		auto ret = std::make_shared<T>(cyclic_buffer[reader_pos]);		
+		value = cyclic_buffer[reader_pos];
 
-		
-		if (reader_pos == writer_pos % 10)
+		if (extra_buffer_in_use)
 		{
+			printf("queue_full, extra_data size:%zu\n", extra_data.size());
 			/*when we suspect that the extra data has info we move it into main buffer*/
 			//check queue
-			std::lock_guard<std::mutex> lk(extra_data_mutex);
+			std::unique_lock<std::mutex> lk(extra_data_mutex);			
+			extra_data_cond.wait(lk, [this] {return (extra_data.size()); });
 			if (extra_data.size())
-			{
-				auto ret = extra_data.front();
+			{	
+				auto tmp = extra_data.front();
 				extra_data.pop();
-				cyclic_buffer[reader_pos] = ret;
+				cyclic_buffer[reader_pos] = tmp;
+				reader_pos = next_reader_pos;
+				if (!extra_data.size())
+					extra_buffer_in_use = false;
+			}
+			else
+			{
+				abort();
+				reader_pos = next_reader_pos;
 			}
 		}
 		else
@@ -109,10 +122,13 @@ public:
 			reader_pos = next_reader_pos;
 		}
 
-		return ret;
+		return true;
 
 
 	}
+
+
+//	bool try_pop(T& value)
 
 };
 
@@ -127,11 +143,11 @@ running_rt_queue::~running_rt_queue()
 {
 }
 
-
+#define RT_QUEUE_SIZE (10)
 void running_rt_queue::operator()()
 {
 	rt_queue<rt_queue_data> main_queue;// = new rt_queue<rt_queue_data>();
-	main_queue.init(10);
+	main_queue.init(RT_QUEUE_SIZE);
 	
 
 	/*
@@ -145,6 +161,7 @@ void running_rt_queue::operator()()
 		std::string data;
 
 		/*this is only for testing burst*/
+
 		for (int i = 0; i < 20; i++)
 		{
 			data = "burst packets " + std::to_string(i);
@@ -163,10 +180,10 @@ void running_rt_queue::operator()()
 	std::thread consumer([&]() {		
 		while (true)
 		{
+			rt_queue_data obj;
 			std::this_thread::sleep_for(std::chrono::milliseconds(1));
-			std::shared_ptr<rt_queue_data> obj = main_queue.pop();
-			if (obj)
-				std::cout << " object received: "<< obj->data <<std::endl;
+			if( main_queue.try_pop(obj))			
+				std::cout << " object received: "<< obj.data <<std::endl;
 		}
 		
 	});
